d0699c0ea931a345f0d873c8350850102d861d8c
RohanVashisht003 <sharmaohan544@gmail.com>
2025-10-03 11:14:27 +0530

    basic setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+    <div className="">
+      <Header />
+      
+
+    </div>
+  );
+}
