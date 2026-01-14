41f5edf222da6932032ed59d9faa94746fa8a127
Rohan <rohan@Rohansharma-MacBook-Air.local>
2026-01-14 16:50:55 +0530

    delete garbage

--- actions/createToken.ts
+++
@@ -1,8 +0,0 @@
-"use server"
-
-import {serverClient} from "@/lib/streamServer"
-
-export async function createToken(userId: string){
-    const token = serverClient.createToken(userId)
-    return token    
-}
